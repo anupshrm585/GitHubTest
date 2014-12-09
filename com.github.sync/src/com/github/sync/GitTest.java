5af96f3130e700de0d3093f5eeee340dba1762ef
Anup Sharma <anupshrm585@gmail.com>
2014-12-09 12:14:16 +0530

    Replied from desktop

--- com.github.sync/src/com/github/sync/GitTest.java
+++ com.github.sync/src/com/github/sync/GitTest.java
@@ -2,6 +2,6 @@ package com.github.sync;
 
 public class GitTest {
 	  public static void main(String[] args) {
-	    System.out.println("Git is great in second machine also");
+	    System.out.println("YES I AM REPLYING");
 	  }
 	} 
